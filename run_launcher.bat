2686355fdc11a138188482bd2f7d994398063652
toncek37 <antoninblaha99@gmail.com>
2026-08-15 14:47:51 +0200

    Launch NeuroChess launcher without console window

--- run_launcher.bat
+++ run_launcher.bat
@@ -2,19 +2,19 @@
 setlocal EnableExtensions
 cd /d "%~dp0"
 
-where python >nul 2>nul
+where pythonw >nul 2>nul
 if errorlevel 1 (
-    echo ERROR: Python was not found in PATH.
-    echo Install Python 3 and enable "Add Python to PATH".
-    pause
-    exit /b 1
+    where python >nul 2>nul
+    if errorlevel 1 (
+        echo ERROR: Python was not found in PATH.
+        echo Install Python 3 and enable "Add Python to PATH".
+        pause
+        exit /b 1
+    )
+    start "" /b python -m gui.launcher_app
+    exit /b 0
 )
 
 set "PYTHONPATH=%CD%\python;%PYTHONPATH%"
-python -m gui.launcher_app
-if errorlevel 1 (
-    echo.
-    echo NeuroChess Launcher exited with an error.
-    pause
-    exit /b 1
-)
+start "" pythonw -m gui.launcher_app
+exit /b 0
